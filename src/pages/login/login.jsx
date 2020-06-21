174c16c561cc4baa4c0644e4e77d9e82a09c5844
DmitriyBezm <dmitriybezm@gmail.com>
2020-06-21 22:35:46 +0300

    added page

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Layout,
+  Typography,
+  Button,
+  Input,
+  Card,
+} from 'components';
+import './styles.less';
+
+export function Login() {
+  return (
+    <Layout
+      className="invite-page"
+    >
+      <section className="invite-page__content">
+      </section>
+    </Layout>
+  );
+}
